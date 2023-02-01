f848e579125fa38cc920ad57c9b2beb2c436eef4
Brandon <pacewicbrandon@gmail.com>
2023-02-01 10:32:31 -0800

    Added hardware

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/drive/localizer/Localizer.kt
@@ -0,0 +1,71 @@
+// Copyright (c) Brandon Pacewic
+// SPDX-License-Identifier: MIT
+
+package org.firstinspires.ftc.teamcode.common.drive.localizer
+
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.acmerobotics.roadrunner.localization.ThreeTrackingWheelLocalizer
+import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.qualcomm.robotcore.hardware.HardwareMap
+
+import org.firstinspires.ftc.teamcode.common.hardware.Encoder
+
+/**
+ * Standard tracking wheel localizer implementation assuming the standard configuration of three tracking wheels:
+ *
+ *    /--------------\
+ *    |     ____     |
+ *    |     ----     |
+ *    | ||        || |
+ *    | ||        || |
+ *    |              |
+ *    |              |
+ *    \--------------/
+ *
+ * Adapted from the Roadrunner quickstart.
+ */
+class Localizer(hardwareMap: HardwareMap) : ThreeTrackingWheelLocalizer(
+        listOf(
+                Pose2d(0.0, LATERAL_DISTANCE / 2, 0.0), // left encoder
+                Pose2d(0.0, -LATERAL_DISTANCE / 2, 0.0), // right encoder
+                Pose2d(FORWARD_OFFSET, 0.0, Math.toRadians(90.0)) // center encoder
+        )
+) {
+    companion object {
+        private const val TICKS_PER_REV = 8192 // rev through bore encoder
+        private const val WHEEL_RADIUS = 1.0 // in
+        private const val GEAR_RATIO = 1.0 // output (wheel) speed / input (encoder) speed
+        private const val LATERAL_DISTANCE = 14.0 // in; distance between the left and right wheels
+        private const val FORWARD_OFFSET = 0.0 // in; offset of the lateral wheel
+    }
+
+    private var leftEncoder: Encoder
+    private var rightEncoder: Encoder
+    private var centerEncoder: Encoder
+
+    init {
+        leftEncoder = Encoder(hardwareMap.get(DcMotorEx::class.java, "leftEncoder"))
+        rightEncoder = Encoder(hardwareMap.get(DcMotorEx::class.java, "rightEncoder"))
+        centerEncoder = Encoder(hardwareMap.get(DcMotorEx::class.java, "centerEncoder"))
+    }
+
+    fun encoderTicksToInches(ticks: Double): Double {
+        return WHEEL_RADIUS * 2.0 * Math.PI * GEAR_RATIO * ticks / TICKS_PER_REV
+    }
+
+    override fun getWheelPositions(): List<Double> {
+        return listOf(
+            encoderTicksToInches((leftEncoder.getCurrentPosition()).toDouble()),
+            encoderTicksToInches((rightEncoder.getCurrentPosition()).toDouble()),
+            encoderTicksToInches((centerEncoder.getCurrentPosition()).toDouble())
+        )
+    }
+
+    override fun getWheelVelocities(): List<Double> {
+        return listOf(
+            encoderTicksToInches(leftEncoder.getCorrectedVelocity()),
+            encoderTicksToInches(rightEncoder.getCorrectedVelocity()),
+            encoderTicksToInches(centerEncoder.getCorrectedVelocity())
+        )
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util/DriveUtil.kt
@@ -0,0 +1,30 @@
+// Copyright (c) Brandon Pacewic
+// SPDX-License-Identifier: MIT
+
+package org.firstinspires.ftc.teamcode.common.util
+
+/**
+ * Set of helper constants / functions for running roadrunner with drive motor encoders instead of tracking wheels
+ * and feedforward velocity control.
+ */
+class DriveUtil {
+    companion object {
+        // Drive motor constants.
+        private const val ticksPerRevolutions = 537.6
+        private const val wheelRadius = 1.8898 // in
+        private const val gearRatio = 1.0
+
+        fun driveEncoderTicksToInches(ticks: Double): Double {
+            return (ticks / ticksPerRevolutions) * (wheelRadius * 2 * Math.PI) * gearRatio
+        }
+
+        fun driveRpmToVelocity(rpm: Double): Double {
+            return rpm * gearRatio * 2 * Math.PI * wheelRadius / 60.0
+        }
+
+        fun getMotorVelocityF(ticksPerSecond: Double): Double {
+            // see https://docs.google.com/document/d/1tyWrXDfMidwYyP_5H4mZyVgaEswhOC35gvdmP-V-5hA/edit#heading=h.61g9ixenznbx
+            return 32767 / ticksPerSecond
+        }
+    }
+}
