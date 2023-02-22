5c21c88853b30433dcb18993d66f60eae9442613
Brandon <pacewicbrandon@gmail.com>
2023-02-22 14:27:54 -0800

    Repo reset

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/drive/trajectory/EmptySequenceException.kt
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Brandon Pacewic
-// SPDX-License-Identifier: MIT WITH FIRST-exception
-
-package org.firstinspires.ftc.teamcode.common.drive.trajectory
-
-/**
- * Adapted from the Roadrunner quickstart
- */
-class EmptySequenceException : RuntimeException()
