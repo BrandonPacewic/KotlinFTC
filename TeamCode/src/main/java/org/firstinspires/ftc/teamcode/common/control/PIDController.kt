ae122e66290aee68347080c695ca84351994f8e6
Brandon <pacewicbrandon@gmail.com>
2023-02-27 09:31:15 -0800

    Added term controllers

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/control/PIDController.kt
@@ -0,0 +1,103 @@
+// Copyright (c) Brandon Pacewic
+// SPDX-License-Identifier: MIT WITH FIRST-exception
+
+package org.firstinspires.ftc.teamcode.common.control
+
+import com.qualcomm.robotcore.util.ElapsedTime
+
+/**
+ * Three term time based PID controller.
+ */
+open class PIDController(
+    var kP: Double,
+    var kI: Double,
+    var kD: Double
+) {
+    var currentError = 0.0
+    var lastError = 0.0
+
+    var timeStep = 0.0
+    var lastTime = 0.0
+    var elapsedTime: ElapsedTime? = null
+
+    var maxOutput = 1.0
+    var minOutput = -1.0
+
+    constructor(
+        kP: Double, 
+        kI: Double, 
+        kD: Double, 
+        maxOutput: Double, 
+        minOutput: Double
+    ) : this(kP, kI, kD) {
+        this.maxOutput = maxOutput
+        this.minOutput = minOutput
+    }
+
+    /**
+     * Calculates the output of the PID controller based on the current vs
+     * target position.
+     *
+     * IMPORTANT: Once you use this function for a given situation, you must
+     * call reset() before changing the set of circumstances that this
+     * controller is being used under.
+     */
+    open fun calculate(
+        currentPosition: Double, 
+        targetPosition: Double
+    ): Double {
+        if (elapsedTime == null) {
+            elapsedTime = ElapsedTime()
+            
+            return firstCalculate(currentPosition, targetPosition)
+        }
+
+        timeStep = elapsedTime!!.milliseconds() - lastTime
+        currentError = targetPosition - currentPosition
+
+        val p = kP * currentError
+        val i = kI * currentError * timeStep
+        val d = (kD * (currentError - lastError)) / timeStep
+        var output = p + i + d
+
+        output = when {
+            output > maxOutput -> maxOutput
+            output < minOutput -> minOutput
+            else -> output
+        }
+
+        lastError = currentError
+        lastTime = elapsedTime!!.milliseconds()
+
+        return output
+    }
+
+    open fun firstCalculate(
+        currentPosition: Double, 
+        targetPosition: Double
+    ): Double {
+        // First time calculate is called, we don't have a time step or last error
+        // so we just return the proportional term.
+        val currentError = targetPosition - currentPosition
+        var output = kP * currentError
+
+        output = when {
+            output > maxOutput -> maxOutput
+            output < minOutput -> minOutput
+            else -> output
+        }
+
+        lastError = currentError
+        lastTime = elapsedTime!!.milliseconds()
+
+        return output
+    }
+
+    fun reset() {
+        currentError = 0.0
+        lastError = 0.0
+        timeStep = 0.0
+        lastTime = 0.0
+        elapsedTime = null
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/control/PIDFController.kt
@@ -0,0 +1,86 @@
+// Copyright (c) Brandon Pacewic
+// SPDX-License-Identifier: MIT WITH FIRST-exception
+
+package org.firstinspires.ftc.teamcode.common.control
+
+import com.qualcomm.robotcore.util.ElapsedTime
+
+/**
+ * Four term time based PIDF controller.
+ */
+class PIDFController(
+    kP: Double,
+    kI: Double,
+    kD: Double,
+    var kF: Double
+): PIDController(kP, kI, kD) {
+    constructor(
+        kP: Double,
+        kI: Double,
+        kD: Double,
+        kF: Double,
+        maxOutput: Double,
+        minOutput: Double
+    ): this(kP, kI, kD, kF) {
+        this.maxOutput = maxOutput
+        this.minOutput = minOutput
+    }
+
+    /**
+     * Calculates the output of the PIDF controller based on the current vs
+     * target position.
+     *
+     * IMPORTANT: Once you use this function for a given situation, you must
+     * call reset() before changing the set of circumstances that this
+     * controller is being used under.
+     */
+    override fun calculate(currentPosition: Double, targetPosition: Double): Double {
+        if (elapsedTime == null) {
+            elapsedTime = ElapsedTime()
+
+            return firstCalculate(currentPosition, targetPosition)
+        }
+
+        timeStep = elapsedTime!!.milliseconds() - lastTime
+        currentError = targetPosition - currentPosition
+
+        val p = kP * currentError
+        val i = kI * currentError * timeStep
+        val d = (kD * (currentError - lastError)) / timeStep
+        val f = kF * targetPosition
+        var output = p + i + d + f
+
+        output = when {
+            output > maxOutput -> maxOutput
+            output < minOutput -> minOutput
+            else -> output
+        }
+
+        lastError = currentError
+        lastTime = elapsedTime!!.milliseconds()
+
+        return output
+    }
+
+    override fun firstCalculate(
+        currentPosition: Double, 
+        targetPosition: Double
+    ): Double {
+        currentError = targetPosition - currentPosition
+        
+        val p = kP * currentError
+        val f = kF * targetPosition
+        var output = p + f
+
+        output = when {
+            output > maxOutput -> maxOutput
+            output < minOutput -> minOutput
+            else -> output
+        }
+
+        lastError = currentError
+        lastTime = elapsedTime!!.milliseconds()
+
+        return output
+    }
+}
