2d15183e6c8085a11ace7863632598548a6c7475
Brandon <pacewicbrandon@gmail.com>
2023-03-02 17:17:02 -0800

    Teleop added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/commands/TeleopTransferCommand.kt
@@ -0,0 +1,16 @@
+// Copyright (c) Brandon Pacewic
+// SPDX-License-Identifier: MIT WITH FIRST-exception
+
+package org.firstinspires.ftc.teamcode.common.commandbase.commands
+
+import com.arcrobotics.ftclib.command.InstantCommand
+import com.arcrobotics.ftclib.command.SequentialCommandGroup
+
+import org.firstinspires.ftc.teamcode.common.drive.localization.TwoWheelLocalizer
+import org.firstinspires.ftc.teamcode.common.hardware.Robot
+
+class TeleopTransferCommand(robot: Robot) : SequentialCommandGroup(
+    InstantCommand({ 
+        TwoWheelLocalizer.imuOffset = robot.localizer!!.getHeading()
+    })
+)
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/OpMode.kt
@@ -0,0 +1,40 @@
+// Copyright (c) Brandon Pacewic
+// SPDX-License-Identifier: MIT WITH FIRST-exception
+
+package org.firstinspires.ftc.teamcode.opmode.teleop
+
+import com.arcrobotics.ftclib.command.CommandOpMode
+import com.arcrobotics.ftclib.command.CommandScheduler
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+
+import org.firstinspires.ftc.teamcode.common.drive.geometry.Pose
+import org.firstinspires.ftc.teamcode.common.hardware.Robot
+
+@TeleOp(name = "OpMode")
+class OpMode : CommandOpMode() {
+    lateinit var robot: Robot
+
+    override fun initialize() {
+        CommandScheduler.getInstance().reset()
+        
+        robot = Robot(hardwareMap)
+        robot.startImuThread(this)
+    }
+
+    override fun run() {
+        robot.drive.setDrivePower(
+            Pose(
+                -gamepad1.left_stick_x.toDouble(),
+                -gamepad1.left_stick_y.toDouble(),
+                -gamepad1.right_stick_x.toDouble()
+            ).times(0.8 - (0.6 * gamepad1.right_trigger.toDouble()))
+        )
+
+        CommandScheduler.getInstance().run()
+    }
+
+    override fun reset() {
+        CommandScheduler.getInstance().reset()
+        robot.reset()
+    }
+}
